ca0bffb71474a2f43ef42ce4088ec03f7845dc0f
empyrosx <empyrosx@yandex.ru>
2017-05-27 00:55:18 +0300

    Add calculating logic.

--- app/src/main/java/com/github/empyrosx/tipcalculator/MainActivity.java
+++ app/src/main/java/com/github/empyrosx/tipcalculator/MainActivity.java
@@ -1,13 +1,113 @@
 package com.github.empyrosx.tipcalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // currency and percent formatter objects
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+
+    private double billAmount = 0.0;
+    private double percent = 0.15;
+    private TextView amountTextView;
+    private TextView percentTextView;
+    private TextView tipTextView;
+    private TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amountTextView = (TextView) findViewById(R.id.amountTextView);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        tipTextView.setText(currencyFormat.format(0));
+        totalTextView.setText(currencyFormat.format(0));
+
+        EditText amountEditText = (EditText) findViewById(R.id.amountEditText);
+        amountEditText.addTextChangedListener(createAmountEditTextWatcher());
+
+        SeekBar percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
+        percentSeekBar.setOnSeekBarChangeListener(createSeekBarListener());
     }
+
+    /**
+     * Calculates and displays tip and total amounts
+     */
+    private void calculate() {
+        percentTextView.setText(percentFormat.format(percent));
+
+        double tip = billAmount * percent;
+        double total = billAmount + tip;
+
+        tipTextView.setText(currencyFormat.format(tip));
+        totalTextView.setText(currencyFormat.format(total));
+    }
+
+    /**
+     * Creates listener object for the SeekBar's progress changed events.
+     */
+    @NonNull
+    private SeekBar.OnSeekBarChangeListener createSeekBarListener() {
+        return new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                percent = progress / 100.0;
+                calculate();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        };
+    }
+
+    /**
+     * Creates listener object for the EditText's text-changed events.
+     */
+    @NonNull
+    private TextWatcher createAmountEditTextWatcher() {
+        return new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                try {
+                    billAmount = Double.parseDouble(s.toString()) / 100.0;
+                    amountTextView.setText(currencyFormat.format(billAmount));
+                } catch (NumberFormatException e) {
+                    amountTextView.setText("");
+                    billAmount = 0.0;
+                }
+
+                calculate();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+        };
+    }
+
+
 }
